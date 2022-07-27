aa831f3845d344cf3833c0542be00859f92b2912
karlygeorge <53354297+karlygeorge@users.noreply.github.com>
2022-07-27 11:11:52 +0100

    Updated the newsletter signup
    Added an alert and cleared the email box after the submit button is clicked

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 $("input[type='email']").blur(function() {
 	if ($(this).val() !== '') {
 		$(this).addClass('hasValue');
+        alert("Thank you for signing up for our newsletter!");
+        email.value = " ";
 	} else {
 		$(this).removeClass('hasValue');
 	}
